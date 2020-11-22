18f123235313c8b7b56f3280105ecdbc5f5cfc40
CatalystGithub <74880451+CatalystGithub@users.noreply.github.com>
2020-11-23 02:33:00 +0300

    test4

--- PoePlugin/PluginSettings.cs
+++ PoePlugin/PluginSettings.cs
@@ -7,5 +7,15 @@ namespace PoePlugin
     public class PluginSettings : ISettings
     {
         public ToggleNode Enable { get; set; }
+        public ToggleNode TestNode1 { get; set; }
+        public ToggleNode TestNode2 { get; set; }
+        public ToggleNode TestNode3 { get; set; }
+
+        public PluginSettings()
+        {
+            Enable = new ToggleNode(true);
+            TestNode1 = new ToggleNode(true);
+            TestNode2 = new ToggleNode(false);
+        }
     }
 }
