f6cbe520345e88452501be67bbeb9656c40270aa
CatalystGithub <74880451+CatalystGithub@users.noreply.github.com>
2020-11-23 02:27:00 +0300

    test3

---
+++ PoePlugin/PluginSettings.cs
@@ -0,0 +1,11 @@
+﻿using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+using SharpDX;
+
+namespace PoePlugin
+{
+    public class PluginSettings : ISettings
+    {
+        public ToggleNode Enable { get; set; }
+    }
+}
